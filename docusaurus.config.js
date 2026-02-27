cfccf7bbe11c2cf2b8604f0287bd9a9a337f87f2
tjgrace7 <121319122+tjgrace7@users.noreply.github.com>
2026-02-27 15:27:41 -0600

    Changed Site Navigation to Dashboard

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,8 +92,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Documentation',
+                to: '/docs/Lease-Link-Pages/Dashboard',
               },
             ],
           },
